30bd3f39952cb9a7f771ed6ac765d2fc6a75b5de
Sylvain Viollon <thefunny@gmail.com>
2011-01-27 17:28:39 +0100

    Activate the JS included in zeam.form.ztk to get the advanced list widget.

--- src/zeam/form/plone/resources/widgets.js
+++ src/zeam/form/plone/resources/widgets.js
@@ -1,43 +1,52 @@
 
+(function ($){
 
-function moveBetweenSelect(sourceId, targetId) {
-    var target = jq('#' + targetId);
+    var moveBetweenSelect = function (sourceId, targetId) {
+        var target = $('#' + targetId);
 
-    jq('#' + sourceId + ' :selected').each(function(count, selected) {
-        var node = jq(selected);
+        $('#' + sourceId + ' :selected').each(function(count, selected) {
+            var node = $(selected);
 
-        node.remove();
-        node.appendTo(target);
-    });
-};
+            node.remove();
+            node.appendTo(target);
+        });
+    };
 
+    $(document).ready(function(){
 
-jq(document).ready(function(){
+        $('.zeam-form').submit(function() {
+            $(this).find('.field-plone-multi-select').each(
+                function(count, node) {
+                    var selectedId = $(node).attr('id') + '-selected';
+                    var allOptions = [];
 
-    jq('.zeam-form').submit(function() {
-        jq(this).find('.field-plone-multi-select').each(
-            function(count, node) {
-                var selectedId = jq(node).attr('id') + '-selected';
-                var allOptions = [];
-                jq('#' + selectedId + ' option').each(function(count, option) {
-                    allOptions[count] = jq(option).attr('value');
+                    $('#' + selectedId + ' option').each(function(count, option) {
+                        allOptions[count] = $(option).attr('value');
+                    });
+                    $('#' + selectedId).val(allOptions);
                 });
-                jq('#' + selectedId).val(allOptions);
+        });
+
+        $('.zeam-form .field-plone-multi-select').each(function(count, node) {
+            var nodeId = $(node).attr('id');
+            var selectedId = nodeId + '-selected';
+            var availableId = nodeId + '-available';
+
+            $('#' + nodeId + '-add').click(function() {
+                moveBetweenSelect(availableId, selectedId);
+                return false;
             });
+            $('#' + nodeId + '-remove').click(function() {
+                moveBetweenSelect(selectedId, availableId);
+                return false;
+            });
+        });
+
+        // On list widget, add class allowMultiSubmit
+        $('.zeam-form input.field-list-add-line').addClass('allowMultiSubmit');
+        $('.zeam-form input.field-list-remove-line').addClass('allowMultiSubmit');
+        $('.zeam-form button.field-list-move-up').addClass('allowMultiSubmit');
+        $('.zeam-form button.field-list-move-down').addClass('allowMultiSubmit');
     });
 
-    jq('.zeam-form .field-plone-multi-select').each(function(count, node) {
-        var nodeId = jq(node).attr('id');
-        var selectedId = nodeId + '-selected';
-        var availableId = nodeId + '-available';
-
-        jq('#' + nodeId + '-add').click(function() {
-            moveBetweenSelect(availableId, selectedId);
-            return false;
-        });
-        jq('#' + nodeId + '-remove').click(function() {
-            moveBetweenSelect(selectedId, availableId);
-            return false;
-        });
-    });
-});
+})(jQuery);
